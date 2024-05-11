c59767884c3bf71d6127c7ed3e08d5d19056ecb0
Andrii Kurdiumov <kant2002@gmail.com>
2024-05-12 01:24:53 +0500

    Додани приклади структури даних для проблеми 1 із інструкції

--- FlowMatic/ОписПоля.cs
+++ FlowMatic/ОписПоля.cs
@@ -5,8 +5,13 @@ namespace FlowMatic;
 public class ОписПоля
 {
     public required string Назва;
-    public ushort Місце;
-    public ТипПоля Тип;
+    public required ushort Місце;
+    public required ТипПоля Тип;
+    public required string ПозіціяТочки;
+    public required byte? ПозіціяЗнака;
+    public required byte ПозіціяНайлівішогоСимвола;
+    public required byte КількістьСимволів;
+    public string Витягач = "000000000000";
 
     public override string ToString()
     {
@@ -18,11 +23,24 @@ public class ОписПоля
         };
         var результат = $@"{Назва,-12}
 000{Місце:D3}000000
-00000{типПоля}xxxxx0
-            
+00000{типПоля}{ПозіціяТочки,-2}{ДоБази12(ПозіціяЗнака)}{ДоБази12(ПозіціяНайлівішогоСимвола)}{ДоБази12(КількістьСимволів)}0
+{Витягач,-12}
 ".ReplaceLineEndings();
         var довжинаРядка = 12 + Environment.NewLine.Length;
         Debug.Assert(результат.Length % довжинаРядка == 0);
         return результат;
+    }
+
+    static char ДоБази12(byte? значення)
+    {
+        if (!значення.HasValue) return ' ';
+
+        return значення.Value switch
+        {
+            > 0 and < 9 => (char)('0' + значення),
+            10 => 'A',
+            11 => 'B',
+            12 => 'C',
+        };
     }
 }
