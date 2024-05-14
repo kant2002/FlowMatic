bec731a1f82e51e932d6e037fae2ec25eef2bd90
Andrii Kurdiumov <kant2002@gmail.com>
2024-05-14 08:11:05 +0500

    Додав трішки віртуальної машини

---
+++ FlowMatic/ВіртуальнаМашина.cs
@@ -0,0 +1,29 @@
+﻿using FlowMatic.Операції;
+using FlowMatic.Юнівак;
+
+namespace FlowMatic;
+
+public class ВіртуальнаМашина
+{
+    private СервоПривід[] СервоПривіди = new СервоПривід[16];
+
+    private Dictionary<char, int> СервоПривідиСловник = new Dictionary<char, int>();
+    private Dictionary<char, string> ФайловіБуфери = new Dictionary<char, string>();
+
+    public СервоПривід ВзятиСервоПривід(char файл)
+    {
+        return СервоПривіди[СервоПривідиСловник[файл]];
+    }
+
+    private int позиція;
+    public void ВиконатиПрограму(ЛентаВводу лентаВводу)
+    {
+        позиція = 0;
+        var компілятор = new Компілятор();
+        var програма = компілятор.Скомпілювати(лентаВводу.ІсходнийКод);
+        while (true)
+        {
+            var поточнаКоманда = програма.Операції[позиція];
+        }
+    }
+}
---
+++ FlowMatic/СервоПривід.cs
@@ -0,0 +1,36 @@
+﻿namespace FlowMatic;
+
+public class СервоПривід
+{
+    private int позиція;
+    private string дані;
+    public bool КінецьДаних;
+    public void ВставитиЛенту(string дані)
+    {
+        this.дані = дані;
+        this.позиція = 0;
+    }
+
+    public string Прочитати(int кількість)
+    {
+        if (this.позиція + кількість >= this.дані.Length)
+        {
+            КінецьДаних = true;
+        }
+
+        кількість = Math.Min(this.дані.Length - 1, this.позиція + кількість);
+        return дані.Substring(this.позиція, кількість);
+    }
+
+    public void Записати(string дані)
+    {
+        this.дані += дані;
+        this.позиція += дані.Length;
+    }
+
+    public void Перемотати()
+    {
+        this.позиція = 0;
+        КінецьДаних = false;
+    }
+}
