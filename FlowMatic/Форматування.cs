cf3bed07a35dca6dde2cff360191e53ba0f0cb61
Andrii Kurdiumov <kant2002@gmail.com>
2024-05-19 18:57:46 +0500

    Додав форматування даних які не повністю запонюють слово

--- FlowMatic/Форматування.cs
+++ FlowMatic/Форматування.cs
@@ -11,10 +11,26 @@ public class Форматування
     public static string Переформатувати(string текст)
     {
         System.Text.StringBuilder результат = new();
-        var chunkSize = 12 + Environment.NewLine.Length;
+        текст = текст.ReplaceLineEndings("");
+        var chunkSize = 12;
         for (var i = 0; i < текст.Length / chunkSize; i++)
         {
             var chunk = текст.Substring(i * chunkSize, 12);
+            результат.Append(chunk);
+            if (i % 10 == 9)
+            {
+                результат.Append(Environment.NewLine);
+            }
+            else
+            {
+                результат.Append(' ');
+            }
+        }
+
+        if (текст.Length % chunkSize != 0)
+        {
+            var i = 1 + (текст.Length / chunkSize);
+            var chunk = текст.Substring((текст.Length / chunkSize) * chunkSize);
             результат.Append(chunk);
             if (i % 10 == 9)
             {
